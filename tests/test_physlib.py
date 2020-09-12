609f1481268e378231cd68324bc836ffb692f50c
Hank Wikle <henry.wikle@gmail.com>
2020-09-11 19:09:00 -0600

    rewrote test file to work with new directory structure

--- tests/test_physlib.py
+++ tests/test_physlib.py
@@ -1,16 +1,16 @@
 from unittest import TestCase
-from physics.physlib import *
+from physics.core import *
 import numpy as np
 
 class TestCoreLib(TestCase):
 
-    def test_Point_raises_TypeError_when_passed_nonarray(self):
+    def test_Particle_raises_TypeError_when_passed_nonarray(self):
         with self.assertRaises(TypeError):
-            pt = Point(3, (1,2,3))
+            pt = Particle(3, (1,2,3))
 
-    def test_Point_raises_ValueError_on_array_with_mismatched_dimension(self):
+    def test_Particle_raises_ValueError_on_array_with_mismatched_dimension(self):
         with self.assertRaises(ValueError):
-            pt = Point(2, np.array([1,2,3]))
+            pt = Particle(2, np.array([1,2,3]))
 
 if __name__ == '__main__':
     unittest.main()
