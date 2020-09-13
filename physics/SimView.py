adf63e2fac1dc79448d3f12745815c2811c29a31
Hank Wikle <henry.wikle@gmail.com>
2020-09-12 19:50:14 -0600

    rewrote SimView to drive SimFrame

--- physics/SimView.py
+++ physics/SimView.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 class SimView(object):
     def __init__(self):
         self.simFrame = None
         self.fig = plt.gcf()
-        self.axes = plt.gca()
-        self.xlim = None
-        self.ylim = None
 
-    def render(self):
-        plt.clf() # clear figure
+    def nextFrame(self, frame):
+        self.simFrame.step()
+        #plt.clf()
 
         for c in self.simFrame.children:
-            plt.plot(c.position[0], c.position[1], 'o')
+            #plt.plot(c.position[0], c.position[1], 'o')
+            plt.plot(self.simFrame.time, c.position[1], "o")
+            
+
+    def start(self):
+        anim = FuncAnimation(self.fig, self.nextFrame)
+        plt.show()
+        
