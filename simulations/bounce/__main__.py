626da11a2ea6d315b6bf421887ea3c587fb7a561
Hank Wikle <henry.wikle@gmail.com>
2020-09-18 14:59:16 -0600

    rewrote bounce to work with extendable SimView object

--- simulations/bounce/__main__.py
+++ simulations/bounce/__main__.py
@@ -1,8 +1,24 @@
-import sys
-sys.path.append("~/Documents/programming/python/physics/physics")
-
 from physics.core import *
 from physics.SimView import *
+from physics.SimWindow import *
+from matplotlib import pyplot as plt
+
+class PositionView(SimView):
+    def __init__(self):
+        super().__init__()
+
+    def next_frame(self):
+        self.axis.clear()
+
+        for c in self.simFrame.children:
+            self.axis.plot(c.position[0], c.position[1], 'bo')
+
+class EnergyView(SimView):
+    def __init__(self):
+        super().__init__()
+
+    def next_frame(self):
+        self.axis.plot(self.simFrame.time, self.simFrame.calc_energy(), 'ro')
 
 # create and configure simulation frame
 simFrame = PhysicsFrame()
@@ -18,11 +34,19 @@ gravity.is_constant = True
 gravity.vector = np.array([0, -G*ball.mass])
 ball.add_force(gravity)
 
-# attach view to simulation frame
-view = SimView()
-view.simFrame = simFrame # implement
+# create simulation window
+window = SimWindow(1, 2)
 
-view.start()
+# attach view to simulation frame
+pos_view = PositionView()
+pos_view.simFrame = simFrame
+window.add_view(pos_view)
+
+energy_view = EnergyView()
+energy_view.simFrame = simFrame
+window.add_view(energy_view)
+
+window.start()
 
 #def animFunc(frame):
 #    simFrame.step()
